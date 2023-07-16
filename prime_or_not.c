598aed3da4917f1b9d1cb0c9687da61f65348c69
Arjunram Pal <44159014+Arjunram-pal@users.noreply.github.com>
2023-07-16 17:11:52 +0530

    Add files via upload

---
+++ prime_or_not.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int x;
+    printf("Enter a number:");
+    scanf("%d",&x);
+    if (x%2==0)
+    {
+        printf("%d is not prime number.");
+    }
+    else
+    {
+        printf("%d is prime number.");
+    }
+    
+}
+//need to solving again
